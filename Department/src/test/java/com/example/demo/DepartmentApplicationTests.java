bb65b81bde2945c80c4efe805ce0d479640549ff
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2022-03-06 22:56:04 +0200

    Test Commit

--- Department/src/test/java/com/example/demo/DepartmentApplicationTests.java
+++ Department/src/test/java/com/example/demo/DepartmentApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,12 @@ class DepartmentApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isEqualTo(true);
 	}
 
+	
+	@Test
+	void theTest() {
+		assertThat(true).isEqualTo(true);
+	}
 }
